07cbe947164b22f1fccf123c1b64bdd6a47cb87f
Antonio Skorin <antonio.skorin@gmail.com>
2023-10-05 01:11:42 -0300

    Added Direccion class and implementation.

---
+++ src/main/java/org/example/Direccion.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Direccion {
+    private String direccion;
+    public Direccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Direccion{" +
+                "direccion='" + direccion + '\'' +
+                '}';
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
